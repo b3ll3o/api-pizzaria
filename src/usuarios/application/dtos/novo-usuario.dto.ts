251d8d36cffe04138049a7818d4d7524e82660b6
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2021-11-20 22:29:14 -0300

    lint

--- src/usuarios/application/dtos/novo-usuario.dto.ts
+++ src/usuarios/application/dtos/novo-usuario.dto.ts
@@ -1,11 +1,11 @@
-import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class NovoUsuarioDto {
-  @IsNotEmpty({ message: 'Email inválido!'})
-  @IsEmail(undefined, { message: 'Email inválido!'})
+  @IsNotEmpty({ message: 'Email inválido!' })
+  @IsEmail(undefined, { message: 'Email inválido!' })
   email: string;
 
   @IsNotEmpty()
-  @MinLength(5, { message: 'A senha deve ter pelo menos 5 caracteres!'})
+  @MinLength(5, { message: 'A senha deve ter pelo menos 5 caracteres!' })
   senha: string;
 }
