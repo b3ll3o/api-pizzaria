ec8283f3a5923c9a8f4f6d3021f35a8caa4ff695
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2021-11-21 18:41:22 -0300

    configurando para ser um unico email no banco de dados'

--- src/auth/jwt/jwt-auth.guard.ts
+++ src/auth/jwt/jwt-auth.guard.ts
@@ -1,7 +1,7 @@
-import { ExecutionContext, Injectable } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
-import { AuthGuard } from "@nestjs/passport";
-import { IS_NO_JWT_GUARD } from "../no-jwt-guard/no-jwt-guard.strategy";
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { IS_NO_JWT_GUARD } from '../no-jwt-guard/no-jwt-guard.strategy';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -10,14 +10,13 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   }
 
   canActivate(context: ExecutionContext) {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_NO_JWT_GUARD, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    
-    if (isPublic) 
-      return true;
-    
+    const isPublic = this.reflector.getAllAndOverride<boolean>(
+      IS_NO_JWT_GUARD,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (isPublic) return true;
+
     return super.canActivate(context);
   }
 }
--- src/usuarios/domain/entities/usuario.entity.ts
+++ src/usuarios/domain/entities/usuario.entity.ts
@@ -4,7 +4,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 export class Usuario {
   @PrimaryGeneratedColumn()
   id: number;
-  @Column()
+  @Column({ unique: true })
   email: string;
   @Column()
   senha: string;
